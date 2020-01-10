78c17895cdc17478018afbdf06fb4fc7604ec3d8
Renan Bezerra <renanbg90@gmail.com>
2020-01-10 15:11:07 -0300

    desafio04 completo

--- src/aula3/Carro.java
+++ src/aula3/Carro.java
@@ -1,73 +1,73 @@
 package aula3;
 
-public class Carro extends Veiculo{
-	
+public class Carro extends Veiculo {
+
 //atributos
 	private int portas;
 	private int ano;
-	
-	
+
 //getters e setters
 
 	public int getPortas() {
 		return portas;
 	}
+
 	public void setPortas(int portas) {
 		this.portas = portas;
 	}
+
 	public int getAno() {
 		return ano;
 	}
+
 	public void setAno(int ano) {
 		this.ano = ano;
 	}
 
 //construtor
 	public Carro() {
-		
+
 	}
+
 	public Carro(String modelo, String marca, String placa, String cor, int km, boolean isLigado, int litrosCombustivel,
 			int velocidade, double preco, int portas, int ano) {
 		super(modelo, marca, placa, cor, km, isLigado, litrosCombustivel, velocidade, preco);
-		
-		
+
 		this.portas = portas;
 		this.ano = ano;
 	}
-	
-	
-@Override
+
 	public void frear() {
-		// TODO Auto-generated method stub
+
 		super.frear();
 	}
-	@Override
+
 	public void pintar(String cor) {
-		// TODO Auto-generated method stub
+
 		super.pintar(cor);
 	}
-	@Override
+
 	public void ligar() {
-		// TODO Auto-generated method stub
+
 		super.ligar();
 	}
-	@Override
+
 	public void desligar() {
-		// TODO Auto-generated method stub
+
 		super.desligar();
 	}
-	//metodos
+
+	// metodos
 	public void abastecer(int quant) {
 		this.setLitrosCombustivel(quant);
 	}
-	
+
 	public void acelerar() {
-	 super.acelerar();	
+		super.acelerar();
 	}
+
 	public void status() {
 		super.status();
-		System.out.println("Portas: " + this.portas
-				+"\nAno: " + this.ano);
+		System.out.println("Portas: " + this.portas + "\nAno: " + this.ano);
 	}
 }
-
--- src/main.java
+++ src/main.java
@@ -5,7 +5,7 @@ class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Veiculo v1 = new herança.Veiculo("General Motors","Spin","ELW4025","branca", 15.00f, 30.00  );
-	Veiculo v2 = new Veiculo();
+		Veiculo v2 = new Veiculo();
 //		v1.ligar();
 //		v1.pintar("azul");
 //		
